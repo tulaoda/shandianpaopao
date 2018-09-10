e3a0821a196225e7c5b66bb55b0533f8ceac94e7
DingYizhong <272465038@qq.com>
2018-09-10 09:16:30 +0800

    移除无用

--- 后台/NetSDPP/src/main/java/com/ssh/dao/FirstDao.java
+++
@@ -1,8 +0,0 @@
-package com.ssh.dao;
-
-import com.ssh.base.DomainDao;
-import com.ssh.entity.First;
-
-public interface FirstDao extends DomainDao<First,Long> {
-    public First getFirstByOrderId(Long orderId) throws Exception;
-}
